d5a736809636bac5d5973fb2dac1246fe559b927
msuribec <msuribec@eafit.edu.co>
2023-09-30 09:28:04 -0500

    removed unnecesary files

--- test.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-
-subprocess.run("C:\Users\sofia\Documents\2023-2\IA\IANS\venv\Scripts\python.exe C:\Users\sofia\Documents\2023-2\IA\IANS\src\run_clustering.py C:\Users\sofia\Documents\2023-2\IA\IANS\Data\iris.csv")
-
