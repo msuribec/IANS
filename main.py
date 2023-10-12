942e226efb336b54f5e019104650c525f16d3f41
msuribec <37965605+msuribec@users.noreply.github.com>
2023-10-12 09:00:04 -0500

    Removed duplicate package.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.utils import CreateVenv
 
 def main(file_path, args = None):
     c = CreateVenv() #create virtual environment
-    c.install_packages(['scikit-learn','pandas', 'xlrd', 'openpyxl', 'matplotlib' , 'scikit-learn']) #install packages
+    c.install_packages(['scikit-learn','pandas', 'xlrd', 'openpyxl', 'matplotlib']) #install packages
     c.run_file(file_path, args) #run file
 
 if __name__ == "__main__":
