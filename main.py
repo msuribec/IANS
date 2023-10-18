bfec6d15f90ab1eb4bc22a2fc59ba5eb16cc1b19
msuribec <msuribec@eafit.edu.co>
2023-10-17 21:42:10 -0500

    upload new results

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ def main(file_path, args = None):
     c.run_file(file_path, args) #run file with list of arguments
 
 if __name__ == "__main__":
-    main('src/run_all.py', ['Data/iris.csv' ,'not', '2,3,4', ''])
+    main('src/run_all.py', ['Data/iris.csv' ,'Iris', '2,3,4', ''])
     main('src/run_all.py', ['Data/iris.csv' ,'Iris Low Dimension', '2,3,4' , 'low'])
     main('src/run_all.py', ['Data/iris.csv' ,'Iris High Dimension', '2,3,4' , 'high'])
